08d9ca2727dc5ae6ffd93e5c935fea9bda77ccf4
akaidaruma <tmwtfujii428@gmail.com>
2019-06-21 13:19:29 +0900

    update for mikata_move_example01.cpp

---
+++ build/test/CMakeFiles/mikata_move_example01.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fujii/libmikataarm/test/mikata_move_example01.cpp" "/home/fujii/libmikataarm/build/test/CMakeFiles/mikata_move_example01.dir/mikata_move_example01.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../test/../include"
+  "../test/../libdynamixel/include"
+  "../test/../libarm/include"
+  "../test/../thirdparty"
+  "../test/../libdynamixel/libaqua/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/fujii/libmikataarm/build/src/CMakeFiles/mikata.dir/DependInfo.cmake"
+  "/home/fujii/libmikataarm/build/libdynamixel/src/CMakeFiles/dynamixel.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/mikata_move_example01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mikata_move_example01.dir/mikata_move_example01.cpp.o"
+  "mikata_move_example01.pdb"
+  "mikata_move_example01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mikata_move_example01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
